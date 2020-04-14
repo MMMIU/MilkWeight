43d8b4345954aa221b78bf4bf6bc74a0044b3c51
Yifie Miao <ymiao29@wisc.edu>
2020-04-14 21:11:58 +0800

    YM

--- application/Main.java
+++ application/Main.java
@@ -5,7 +5,7 @@ import javafx.application.Application;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Application.launch(test.class, args);
+        Application.launch(GUI.class, args);
     }
 
 }
---
+++ cd.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m"YM"
+git push
