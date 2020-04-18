7432291c10471e7f0c7dbc21a1a32b18fa4b87d9
chenxixixi-xi <61204651+chenxixixi-xi@users.noreply.github.com>
2020-04-18 02:19:23 -0700

    Update FileOutputer.java

--- application/FileOutputer.java
+++ application/FileOutputer.java
@@ -38,5 +38,50 @@ public class FileOutputer {
     		outFile.println(record.getFarmID()+", "+record.getWeight()+", "+100.0*record.getWeight()/totalWeight);
     	}
     }
+	
+    public void farmReport(String farmID,int year,PrintWriter outFile) {
+    	List<String> nameList=this.database.getFarmIDList();
+    	long totalWeight=0;
+    	List<OneRecord> list=new ArrayList<>();
+    	for(String e:nameList) {
+    		OneRecord onemonthFarm=new OneRecord(e, year, 0);
+    		for(OneRecord r:this.database.getAllRecordsInAYear(year)) {
+    			if(r.getFarmID()==farmID) {
+    				onemonthFarm.setWeight(onemonthFarm.getWeight()+r.getWeight());
+    				totalWeight+=r.getWeight();
+    			}
+    		}
+    		list.add(onemonthFarm);
+    	}
+    	outFile.println("Farm ID, Weight, Percent of total");
+    	for(OneRecord record:list) {
+    		outFile.println(record.getFarmID()+", "+record.getWeight()+", "+100.0*record.getWeight()/totalWeight);
+    	}
+    }
+
+    public void monthlyReport(int yearMonth,PrintWriter outFile) {
+    	List<String> nameList=this.database.getFarmIDList();
+    	long totalWeight=0;
+    	List<OneRecord> list=new ArrayList<>();
+    	for(String e:nameList) {
+    		OneRecord oneFarm=new OneRecord(e, 0, 0);
+    		for(OneRecord r:this.database.getAllRecordsOfAFarm(e)) {
+    			if(r.getDate()/1000000==yearMonth) {
+    				oneFarm.setWeight(oneFarm.getWeight()+r.getWeight());
+    				totalWeight+=r.getWeight();
+    			}
+    		}
+    		list.add(oneFarm);
+    	}
+    	outFile.println("Farm ID, Weight, Percent of total");
+    	for(OneRecord record:list) {
+    		outFile.println(record.getFarmID()+", "+record.getWeight()+", "+100.0*record.getWeight()/totalWeight);
+    	}
+    }
+    
+    public void dateRangeReport(int date1,int date2,PrintWriter outFile) {
+    	//using for loop to add the month until from date1 to date2
+    	
+    }
 
 }
